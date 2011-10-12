d026d56fe814197bc86b54357ade8d0d9e24a2e2
d11wtq <chris@w3style.co.uk>
2011-10-12 16:17:31 +1100

    Some documentation on the AbstractRequest and AbstractResponse classes

--- lib/pp-adaptive/abstract_request.rb
+++ lib/pp-adaptive/abstract_request.rb
@@ -1,5 +1,13 @@
 module AdaptivePayments
+  # AbstractRequest is a JsonModel that defines some default behaviour for all requests
+  #
+  # A request defines the operation it executes and provides a method for building the corresponding response.
+  # All requests provide a RequestEnvelope by default.  There is no need to re-define it in descendants.
   class AbstractRequest < JsonModel
+    # The RequestEnvelope, providing the detail_level and error_language.
+    #
+    # Aliases are defined for #detail_level and #error_language.  They do not need to be accessed through the
+    # request_envelope.
     attribute :request_envelope, Node[RequestEnvelope], :param => "requestEnvelope"
 
     alias_params :request_envelope, {
@@ -8,14 +16,28 @@ module AdaptivePayments
     }
 
     class << self
+      # Set or get the API operation for the request class
+      #
+      # @param [Symbol] name
+      #   the name of the operation defined in the Adaptive Payments API, optional
+      #
+      # @return [Symbol]
+      #   the name of the operation defined in the Adaptive Payments API
       def operation(name = nil)
         @operation = name unless name.nil?
         @operation
       end
 
-      def build_response(string)
+      # Given a JSON string, return the corresponding response.
+      #
+      # @param [String] json
+      #   the raw JSON string as returned by the API
+      #
+      # @return [AbstractResponse]
+      #   the corresponding response for the JSON
+      def build_response(json)
         klass = AdaptivePayments.const_get(operation.to_s + "Response")
-        klass.from_json(string.to_s)
+        klass.from_json(json.to_s)
       end
     end
   end
--- lib/pp-adaptive/abstract_response.rb
+++ lib/pp-adaptive/abstract_response.rb
@@ -1,7 +1,18 @@
 module AdaptivePayments
+  # AbstractResponse provides a JsonModel with the default behaviour for all responses
+  #
+  # The ResponseEnvelope is already included by default.  It does not need to be redeclared in descendants.
+  #
+  # All responses have FaultMessage behaviour mixed in, so will behave as a FaultMessage if the API returns one.
   class AbstractResponse < JsonModel
     include FaultMessage
 
+    # The ResponseEnvelope, encapsulating common information about the response
+    #
+    # All attributes #ack_code, #build, #correlation_id and #time are aliased as top-level attributes,
+    # so you do not need to access #response_envelope directly.
+    #
+    # @see #success?, #failure?, #warning?, #success_with_warning?, #failure_with_warning?
     attribute :response_envelope, Node[ResponseEnvelope], :param => "responseEnvelope"
 
     alias_params :response_envelope, {
@@ -12,12 +23,22 @@ module AdaptivePayments
     }
 
     class << self
+      # Set or get the API operation
+      #
+      # This method should be called in descendants, but is not currently used.
+      #
+      # @param [Symbol] name
+      #   the name of the API operation as defined by PayPal, optional
+      #
+      # @return [Symbol]
+      #   the name of the API operation as defined by PayPal
       def operation(name = nil)
         @operation = name unless name.nil?
         @operation
       end
     end
 
+    # Provide status predicates for the possible response ack codes
     %w{success? failure? warning? success_with_warning? failure_with_warning?}.each do |method|
       define_method(method) { responseEnvelope.send(method) }
     end
